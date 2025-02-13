740296195091eb50166a38a0a429ad81aa35a767
pkhrapunova <pkhrapunova@gmail.com>
2025-02-13 18:23:23 +0300

    create classes

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Master master = new Master("sfb","jhg","+987654","jhgfiuytr");
         Client client= new Client();
-        Material material = new Material("hgf",456,Categories.LIPSTICK);
+
         client.scan();
 
         System.out.println(client.toString());
