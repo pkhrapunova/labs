99af525ca0351a60c892be877908a6e52eb25194
Храпунова Полина <pkhrapunova@gmail.com>
2025-03-11 16:21:52 +0300

    finish

--- src/javalabs/lab6/lab6_3/Model/Client.java
+++ src/javalabs/lab6/lab6_3/Model/Client.java
@@ -25,13 +25,4 @@ public class Client extends Person {
         return phoneNumber.matches("[+]?[0-9]{0,13}");
     }
 
-    public static Client fromString(String str) {
-        // Пример строки: "Client: LastName: Smith; FirstName: John; NumberPhone: +123456789;"
-        String[] parts = str.split("; ");
-        String lastName = parts[0].replace("Client: LastName: ", "");
-        String firstName = parts[1].replace("FirstName: ", "");
-        String numberPhone = parts[2].replace("NumberPhone: ", "");
-
-        return new Client(firstName, lastName, numberPhone);
-    }
 }
